36b6aff932ec102d0b321750709bd923a6c11e18
Yajie Deng <yajie.deng@st.oth-regensburg.de>
2025-05-21 11:28:22 +0200

    create a simple form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {DummyComponentComponent} from './dummy-component/dummy-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DummyComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
