fffaa89b1011000d126686864ee161a70f52a906
Yajie Deng <yajie.deng@st.oth-regensburg.de>
2025-05-21 11:40:52 +0200

    *ngIf

--- src/app/dummy-component/dummy-component.component.ts
+++ src/app/dummy-component/dummy-component.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-dummy-component',
   imports: [
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './dummy-component.component.html',
   styleUrl: './dummy-component.component.scss'
@@ -12,8 +14,9 @@ import {FormsModule} from '@angular/forms';
 export class DummyComponentComponent {
   name = "";
   email ="";
+  isSubmitted:Boolean = false;
 
   onSubmit() {
-
+    this.isSubmitted = true;
   }
 }
