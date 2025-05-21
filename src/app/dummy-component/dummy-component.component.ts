5d90757a6fd071957ebc7e1f7df74f69cde42ca2
Yajie Deng <yajie.deng@st.oth-regensburg.de>
2025-05-21 14:52:46 +0200

    *ngFor of. html table.

--- src/app/dummy-component/dummy-component.component.ts
+++ src/app/dummy-component/dummy-component.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {NgIf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-dummy-component',
   imports: [
     FormsModule,
-    NgIf
+    NgIf,
+    NgForOf
   ],
   templateUrl: './dummy-component.component.html',
   styleUrl: './dummy-component.component.scss'
@@ -14,9 +15,17 @@ import {NgIf} from '@angular/common';
 export class DummyComponentComponent {
   name = "";
   email ="";
+  message="";
   isSubmitted:Boolean = false;
+  messages:Array<any> = [];
 
   onSubmit() {
     this.isSubmitted = true;
+    this.messages.push({
+      'name':this.name,
+      'email':this.email,
+      'message':this.message,
+    })
+    console.log("messages:",this.messages);
   }
 }
