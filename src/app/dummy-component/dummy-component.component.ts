bb679ebcc82fc1da1080e9062f488386c5ee4817
Yajie Deng <yajie.deng@st.oth-regensburg.de>
2025-05-21 20:59:36 +0200

    output event. delete

--- src/app/dummy-component/dummy-component.component.ts
+++ src/app/dummy-component/dummy-component.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {NgForOf, NgIf} from '@angular/common';
+import {MessageDetailsComponent} from '../message-details/message-details.component';
 
 @Component({
   selector: 'app-dummy-component',
   imports: [
     FormsModule,
     NgIf,
-    NgForOf
+    NgForOf,
+    MessageDetailsComponent
   ],
   templateUrl: './dummy-component.component.html',
   styleUrl: './dummy-component.component.scss'
@@ -27,5 +29,9 @@ export class DummyComponentComponent {
       'message':this.message,
     })
     console.log("messages:",this.messages);
+  }
+
+  deleteMessage(index: number) {
+    this.messages.splice(index, 1);
   }
 }
